894b5dd05f54a59e0f561e82c59ce2c5a2172f21
faaaqwq <2467862885@qq.com>
2026-03-14 23:13:01 +0800

    pre”
    git commit -m pre”

---
+++ Config.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+
+# 图片及视频检测结果保存路径
+save_path = 'D:'
+
+# 使用的模型路径
+model_path = 'runs/detect/exp/weights/best.pt'
+
+
+names = {0: 'spaghetti', 1:'zits', 2:'stringing'}
+
+CH_names = ['意大利面缺陷', '小疙瘩', '拉丝']
